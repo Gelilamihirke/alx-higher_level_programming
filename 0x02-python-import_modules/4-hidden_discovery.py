d136c552726644583200298d4aedc6d0f4e141c1
gelila mihirke <mihirkelala@gmail.com>
2024-01-04 12:41:43 -0800

    done

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import *
-    hulu = dir()
-    for i in range(0, len(hulu)):
-        if hulu[i][:2] != "__":
-            print("{:s}".format(hulu[i]))
-
+    import hidden_4
+    sem = dir(hidden_4)
+    for sem in sems:
+        if sem[:2] != "__":
+            print(sem)
