c0352fb614018815c8c69132ee6eb3bfac86ce09
gelila mihirke <mihirkelala@gmail.com>
2024-01-17 11:06:57 -0800

    done

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,21 +1,8 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    # Check if matrix is empty or not
-    if matrix:
-        # Get the dimensions of the matrix
-        rows = len(matrix)
-        cols = len(matrix[0])
+    new_matrix = [row[:] for row in matrix]
 
-        # Create a new matrix to store the squared values
-        new_matrix = [[0] * cols for _ in range(rows)]
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x ** 2, matrix[i]))
 
-        # Iterate through each element in the matrix
-        for i in range(rows):
-            for j in range(cols):
-                # Calculate the square of the element and store it in the new matrix
-                new_matrix[i][j] = matrix[i][j] ** 2
-
-        return new_matrix
-    else:
-        # If the input matrix is empty, return an empty matrix
-        return []
+    return new_matrix
