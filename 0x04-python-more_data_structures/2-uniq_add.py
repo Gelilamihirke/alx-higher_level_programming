eb95e05124a6aa7ee02ec8b3a0808205e9e66ea2
gelila mihirke <mihirkelala@gmail.com>
2024-01-17 11:18:47 -0800

    done

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pythod3
+def uniq_add(my_list=[]):
+    unique_numbers = set(my_list)
+    sum_of_unique = 0
+
+    for number in unique_numbers:
+        sum_of_unique += number
+
+    return sum_of_unique
