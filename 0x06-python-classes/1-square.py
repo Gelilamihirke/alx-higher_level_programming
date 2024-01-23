12a545d9c510afbce20b0803abf892eeb7340359
gelila mihirke <mihirkelala@gmail.com>
2024-01-23 13:54:30 -0800

    done

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,12 +3,3 @@ class Square:
 
     def __init__(self, size):
         self.__size = size
-
-    def get_size(self):
-        return self.__size
-
-    def set_size(self, new_size):
-        self.__size = new_size
-
-    def area(self):
-        return self.__size ** 2
