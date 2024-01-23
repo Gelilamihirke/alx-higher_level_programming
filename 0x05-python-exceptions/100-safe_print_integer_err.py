07b24741ed25bebb245e29fae7af0a3fb0282a2b
gelila mihirke <mihirkelala@gmail.com>
2024-01-22 16:30:47 -0800

    done

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
- """
-    Print an integer value.
-
-    Args:
-        value: The value to be printed.
-
-    Returns:
-        True if the value is an integer and successfully printed.
-        False otherwise.
-
-    """
     try:
         print("{:d}".format(value))
-        return (True)
-    except (TypeError, ValueError):
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (False)
+    except (ValueError, TypeError) as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return False
+    else:
+        return True
+
