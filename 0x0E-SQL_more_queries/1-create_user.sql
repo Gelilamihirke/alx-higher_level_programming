93ec02fb746be7010971eb23cd6b1d63b00bf03a
Gelilamihirke <mihirkelala@gmail.com>
2024-03-27 09:31:56 -0700

    creating the sql file and give priv

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- creating my sql server and grant all priviledges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
